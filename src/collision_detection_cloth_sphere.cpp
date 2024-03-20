294e270c73842f410f25078308a4f43801bc971d
Haoyang Wu <nn9.yang@gmail.com>
2024-03-20 09:36:49 +0800

    add: implementation of collision detection and resolution

--- src/collision_detection_cloth_sphere.cpp
+++ src/collision_detection_cloth_sphere.cpp
@@ -5,5 +5,15 @@ void collision_detection_cloth_sphere(std::vector<unsigned int> &cloth_index, st
     cloth_index.clear();
     normals.clear();
 
-    
+    double r2 = radius * radius;
+    int n = q.size() / 3;
+
+    for (int i = 0; i < n; i ++) {
+        Eigen::Vector3d p = q.segment(i*3, 3);
+        Eigen::Vector3d r2p = p - center;
+        if (r2p.dot(r2p) <= r2) {
+            cloth_index.push_back(i);
+            normals.push_back(r2p.normalized());
+        }
+    }
 }
--- src/velocity_filter_cloth_sphere.cpp
+++ src/velocity_filter_cloth_sphere.cpp
@@ -2,7 +2,11 @@
 
 void velocity_filter_cloth_sphere(Eigen::VectorXd &qdot, const std::vector<unsigned int> &indices, 
                                   const std::vector<Eigen::Vector3d> &normals) {
-
-    
-
+    for (int i = 0; i < indices.size(); i ++) {
+        int id = indices[i];
+        Eigen::Vector3d n = normals[i];
+        Eigen::Vector3d vel = qdot.segment(i*3, 3);
+        vel = vel - n.dot(vel) * n;
+        qdot.segment<3>(i) = vel;
+    }
 }
